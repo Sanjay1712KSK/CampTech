55c88377d2002a2e765db1df8681b7334768b901
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-03-18 09:26:52 +0530

    UI created: Flutter

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // Set this to the local network IP reachable from your Android device.
-  static const baseUrl = 'http://192.168.1.10:8000';
+  static const baseUrl = 'http://192.168.0.3:8000';
 }
