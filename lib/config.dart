3e46941f14ef195490ed9bec7b730e686521e925
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-04-03 18:34:47 +0530

    Update : API Documentation

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // Set this to the local network IP reachable from your Android device.
-  static const String baseUrl = 'http://192.168.0.6:8000';
+  static const String baseUrl = 'http://192.168.0.7:8000';
 }
